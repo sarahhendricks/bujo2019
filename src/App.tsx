9cf9db22ad0a185b7d00ff74fb4c5d70fa606ff4
Sarah Hendricks <slynnhendricks@gmail.com>
2019-11-29 15:33:44 -0700

    Determining which divs are in the viewport

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 import { selectPinsByMonth, selectIsLoadingPins } from "./store/pins/selectors";
 import { PinState } from "./store/pins/reducers";
 import { Loader, Dimmer } from "semantic-ui-react";
+import VisibilitySensor from "react-visibility-sensor";
 
 const mapStateToProps = (state: PinState) => ({
     isLoadingPins: selectIsLoadingPins(state),
@@ -17,6 +18,17 @@ const dispatchProps = {
 
 type Props = ReturnType<typeof mapStateToProps> & typeof dispatchProps;
 
+const onVisibilityChange = (isVisible: boolean, month: string) => {
+    console.log(`${month} is visible now: ${isVisible}`);
+};
+
+const resizeImage = (originalHeight: number, originalWidth: number) => {
+    const isHorizontal = originalHeight <= originalWidth;
+    return isHorizontal
+        ? { height: "auto", width: "300px" }
+        : { height: "300px", width: "auto" };
+};
+
 const App: FunctionComponent<Props> = props => {
     useEffect(() => {
         Pinterest.login(() => {
@@ -25,42 +37,42 @@ const App: FunctionComponent<Props> = props => {
         });
     }, []);
 
-    const resizeImage = (originalHeight: number, originalWidth: number) => {
-        const isHorizontal = originalHeight <= originalWidth;
-        return isHorizontal
-            ? { height: "auto", width: "300px" }
-            : { height: "300px", width: "auto" };
-    };
-
     return (
         <div className="App">
             {/* TODO:
-                - Determine which divs are in the viewport
                 - Run the dispatch on the visible divs to grab their content
                 -  */}
-            {months.map(month => (
-                <div id={month} key={month}>
-                    <h1>{month}</h1>
-                    {props.isLoadingPins && !props.pins && (
-                        <Dimmer active inverted>
-                            <Loader />
-                        </Dimmer>
-                    )}
-                    {props.pins &&
-                        props.pins.map(pin => (
-                            <a href={pin.link} key={month + pin.id}>
-                                <img
-                                    className="fadeIn"
-                                    style={resizeImage(
-                                        pin.image.original.height,
-                                        pin.image.original.width
-                                    )}
-                                    src={pin.image.original.url}
-                                    alt={pin.note}
-                                />
-                            </a>
-                        ))}
-                </div>
+            {months.map((month: string) => (
+                <VisibilitySensor
+                    onChange={(isVisible: boolean) =>
+                        onVisibilityChange(isVisible, month)
+                    }
+                    partialVisibility
+                    key={month}
+                >
+                    <div id={month} key={month}>
+                        <h1>{month}</h1>
+                        {props.isLoadingPins && !props.pins && (
+                            <Dimmer active inverted>
+                                <Loader />
+                            </Dimmer>
+                        )}
+                        {props.pins &&
+                            props.pins.map(pin => (
+                                <a href={pin.link} key={month + pin.id}>
+                                    <img
+                                        className="fadeIn"
+                                        style={resizeImage(
+                                            pin.image.original.height,
+                                            pin.image.original.width
+                                        )}
+                                        src={pin.image.original.url}
+                                        alt={pin.note}
+                                    />
+                                </a>
+                            ))}
+                    </div>
+                </VisibilitySensor>
             ))}
         </div>
     );
