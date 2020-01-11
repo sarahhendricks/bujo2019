a5b1f59f685b1c6433a6b9d1c3ff1a4be1de8451
Sarah Hendricks <slynnhendricks@gmail.com>
2020-01-11 13:50:12 -0500

    Lazy loading images.
    Not perfect, but much better. The app is no longer stuck blank for
    multiple seconds while applying images for all the months. However,
    still need to optimize so that images are only applied one at a time to
    the DOM, with the fade and delay.

--- src/Month.tsx
+++ src/Month.tsx
@@ -6,6 +6,7 @@ import { Dimmer, Loader, Segment } from "semantic-ui-react";
 import { Pin } from "./types";
 import uuid from "uuid";
 import { CSSTransition } from "react-transition-group";
+import LazyLoad from "react-lazyload";
 
 type OwnProps = {
     month: string;
@@ -39,6 +40,14 @@ const resizeImage = (originalHeight: number, originalWidth: number) => {
           };
 };
 
+const Loading: FunctionComponent = () => (
+    <Segment inverted>
+        <Dimmer active inverted>
+            <Loader />
+        </Dimmer>
+    </Segment>
+);
+
 const Month: FunctionComponent<Props> = ({ month, isLoadingPins, pins }) => {
     return (
         <div
@@ -47,43 +56,38 @@ const Month: FunctionComponent<Props> = ({ month, isLoadingPins, pins }) => {
             style={{ height: "1400px", width: "1000px" }}
         >
             <h1>{month}</h1>
-            {isLoadingPins && !pins && (
-                <Segment inverted>
-                    <Dimmer active inverted>
-                        <Loader />
-                    </Dimmer>
-                </Segment>
-            )}
-            <div
-                style={{
-                    position: "absolute",
-                    height: "1000px",
-                    width: "100%",
-                    padding: "0 150px 0 0"
-                }}
-            >
-                {pins &&
-                    pins.map((pin: Pin) => (
-                        <CSSTransition
-                            in={true}
-                            timeout={5000}
-                            classNames={"fade"}
-                            key={uuid()}
-                        >
-                            <a href={pin.link}>
-                                <img
-                                    // className="fadeIn"
-                                    style={resizeImage(
-                                        pin.image.original.height,
-                                        pin.image.original.width
-                                    )}
-                                    src={pin.image.original.url}
-                                    alt={pin.note}
-                                />
-                            </a>
-                        </CSSTransition>
-                    ))}
-            </div>
+            <LazyLoad key={month} placeholder={<Loading />}>
+                <div
+                    style={{
+                        position: "absolute",
+                        height: "1000px",
+                        width: "100%",
+                        padding: "0 150px 0 0"
+                    }}
+                >
+                    {pins &&
+                        pins.map((pin: Pin) => (
+                            <CSSTransition
+                                in={true}
+                                timeout={5000}
+                                classNames={"fade"}
+                                key={uuid()}
+                            >
+                                <a href={pin.link}>
+                                    <img
+                                        // className="fadeIn"
+                                        style={resizeImage(
+                                            pin.image.original.height,
+                                            pin.image.original.width
+                                        )}
+                                        src={pin.image.original.url}
+                                        alt={pin.note}
+                                    />
+                                </a>
+                            </CSSTransition>
+                        ))}
+                </div>
+            </LazyLoad>
         </div>
     );
 };
