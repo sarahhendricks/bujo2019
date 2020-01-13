d03bac152657890e641d63fb2917bef05927d5c3
Sarah Hendricks <slynnhendricks@gmail.com>
2020-01-12 21:08:50 -0500

    Fixed the positioning.
    Things are now correct! F I N A L L Y

--- src/Month.tsx
+++ src/Month.tsx
@@ -1,10 +1,9 @@
 import React, { FunctionComponent } from "react";
 import { connect } from "react-redux";
 import { PinState } from "./store/pins/reducers";
-import { selectIsLoadingPins, selectPinsByMonth } from "./store/pins/selectors";
+import { selectPinsByMonth } from "./store/pins/selectors";
 import { Dimmer, Loader, Segment } from "semantic-ui-react";
 import { Pin } from "./types";
-import uuid from "uuid";
 import { CSSTransition } from "react-transition-group";
 import LazyLoad from "react-lazyload";
 
@@ -13,7 +12,6 @@ type OwnProps = {
 };
 
 const mapStateToProps = (state: PinState, ownProps: OwnProps) => ({
-    isLoadingPins: selectIsLoadingPins(state),
     pins: selectPinsByMonth(state, { month: ownProps.month })
 });
 
@@ -21,21 +19,24 @@ type Props = ReturnType<typeof mapStateToProps> & OwnProps;
 
 const resizeImage = (originalHeight: number, originalWidth: number) => {
     const isHorizontal = originalHeight <= originalWidth;
-    const top = Math.floor(Math.random() * 95);
-    const left = Math.floor(Math.random() * 95);
+    var ratio = Math.min(300 / originalWidth, 300 / originalHeight);
     return isHorizontal
         ? {
               height: "auto",
               width: "300px",
-              top: `${top}%`,
-              left: `${left}%`,
+              top: `${Math.floor(
+                  Math.random() * (1000 - originalHeight * ratio)
+              )}px`,
+              left: `${Math.floor(Math.random() * (1000 - 300))}px`,
               position: "absolute" as "absolute"
           }
         : {
               height: "300px",
               width: "auto",
-              top: `${top}%`,
-              left: `${left}%`,
+              top: `${Math.floor(Math.random() * (1000 - 300))}px`,
+              left: `${Math.floor(
+                  Math.random() * (1000 - originalWidth * ratio)
+              )}px`,
               position: "absolute" as "absolute"
           };
 };
@@ -48,30 +49,40 @@ const Loading: FunctionComponent = () => (
     </Segment>
 );
 
-const Month: FunctionComponent<Props> = ({ month, isLoadingPins, pins }) => {
+const Month: FunctionComponent<Props> = ({ month, pins }) => {
     return (
         <div
             id={month}
             key={month}
-            style={{ height: "1400px", width: "1000px" }}
+            style={{
+                height: "1400px",
+                width: "1000px",
+                margin: "0 auto",
+                backgroundColor: "#aabbcc"
+            }}
         >
             <h1>{month}</h1>
             <LazyLoad key={month} placeholder={<Loading />}>
                 <div
                     style={{
-                        position: "absolute",
+                        position: "relative",
                         height: "1000px",
                         width: "100%",
-                        padding: "0 150px 0 0"
+                        padding: "0 150px 0 0",
+                        backgroundColor: "#aaaaaa"
                     }}
                 >
+                    <img
+                        style={resizeImage(300, 340)}
+                        src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg"
+                    />
                     {pins &&
                         pins.map((pin: Pin) => (
                             <CSSTransition
                                 in={true}
                                 timeout={5000}
                                 classNames={"fade"}
-                                key={uuid()}
+                                key={pin.id}
                             >
                                 <a href={pin.link}>
                                     <img
